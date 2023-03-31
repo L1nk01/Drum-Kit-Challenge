2725d9ae49decbe653d6bba968b360e6f041bb31
L1nk01 <ecampospaulino@gmail.com>
2023-03-31 19:23:55 -0400

    First commit of the repository

---
+++ index.js
@@ -0,0 +1,7 @@
+button = document.querySelectorAll(".drum");
+
+for (let i = 0; i < button.length; i++) {
+
+    button[i].addEventListener("click", () => {console.log("presionaste " + button[i].textContent)});
+
+}
